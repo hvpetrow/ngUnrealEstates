6d2c43ee3a0e12b1bf9f85003b7d526868f9171f
= <hvpetrow@gmail.com>
2023-03-29 19:48:04 +0200

    spinner initialized

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstateCardComponent } from './estate-card/estate-card.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
-  declarations: [EstateCardComponent],
+  declarations: [EstateCardComponent, SpinnerComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    EstateCardComponent
+    EstateCardComponent,
+    SpinnerComponent
   ]
 })
 export class SharedModule { }
